c45fba0e239325c7e394b9d375dd9d1c18e027c9
Moeketsi Tsotetsi <tmoeketsid@gmail.com>
2023-06-28 23:00:22 +0200

    Added shop section and picture background

---
+++ SeneakerKop/Pages/Info/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SeneakerKop.Pages.Info
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SeneakerKop/Pages/ProductDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SeneakerKop.Pages
+{
+    public class ProductDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
